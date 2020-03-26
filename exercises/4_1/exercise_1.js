3b7cedb43ee06b80723eff0d18216f01a526e477
Diego Matheus Campos <diegodiko05@gmail.com>
2020-03-26 17:50:24 -0300

    resolucao do exercicio numero 1

---
+++ exercises/4_1/exercise_1.js
@@ -0,0 +1,18 @@
+let num1 = 12;
+let num2 = 5;
+let resultado;
+
+//Programa para adição
+resultado = num1+num2
+
+//Programa para subtração
+resultado = num1 - num2;
+
+//Programa para Multiplicação
+resultado =  num1 * num2;
+
+//Programa para divisão
+resultado = num1 / num1;
+
+//Programa para modulo
+resultado = num1 % num2;
