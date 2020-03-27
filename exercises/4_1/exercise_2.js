0d87bd80d9a7a1a402a0d3cd05cce7f147a13daf
Diego Matheus Campos <diegodiko05@gmail.com>
2020-03-27 12:42:37 -0300

    Resolucao do exercicio 2

---
+++ exercises/4_1/exercise_2.js
@@ -0,0 +1,8 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let num1 = 11;
+let num2 = 10;
+
+
+let resultado = (num1 > num2 ? num1 : num2 );
+console.log (`O maior número é o ${resultado}`)
