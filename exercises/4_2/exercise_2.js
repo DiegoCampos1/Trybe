2e67f94432bf3692016a96ac24763826d5dce86d
Diego Matheus Campos <diegodiko05@gmail.com>
2020-03-30 10:58:22 -0300

    Resolution 1,2 and 3

---
+++ exercises/4_2/exercise_2.js
@@ -0,0 +1,8 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+let n = 10;
+let asteriscos = '';
+
+for (c = 0 ; c < n ; c+=1){
+asteriscos += '*'
+console.log(asteriscos);
+}
