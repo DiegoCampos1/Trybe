222d26468b971c611fe8a567f33987f72da29719
Diego Matheus Campos <diegodiko05@gmail.com>
2020-03-30 18:13:54 -0300

    Step 2

--- exercises/4_3/exercise_1.js
+++ exercises/4_3/exercise_1.js
@@ -2,13 +2,14 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "sim"
 };
 
 //Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-
 function imprimaNome (a) {
-  return "Bem-vindo(a)" + a
+  return "Bem-vindo(a) " + a
 }
-console.log(imprimaNome(info.personagem))
+console.log(imprimaNome(info.personagem));
 
-//
+//Insira no objeto uma nova propriedade com o nome de chave “recorrente” e o valor “Sim” e, em seguida, imprima o objeto no console.
+console.log(info);
