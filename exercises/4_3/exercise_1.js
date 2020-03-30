c176dcc243c8566456d3f7fb58143b9339455642
Diego Matheus Campos <diegodiko05@gmail.com>
2020-03-30 18:10:36 -0300

    Step 1

---
+++ exercises/4_3/exercise_1.js
@@ -0,0 +1,14 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+function imprimaNome (a) {
+  return "Bem-vindo(a)" + a
+}
+console.log(imprimaNome(info.personagem))
+
+//
