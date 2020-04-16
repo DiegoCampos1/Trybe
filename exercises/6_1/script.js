416aad84334c8d794285b089718303e5a71a839d
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-16 11:58:54 -0300

    validade e-mail

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,21 +1,33 @@
 const mail = document.querySelector('#email')
 const button1 = document.querySelector('#button1')
+const uf = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ]; 
 
+const estado = document.querySelector('#select')
 
-function ValidateEmail(inputText) {
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
- if(inputText.value.match(mailformat))
-{
-document.form1.text1.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
+for (let i = 0; i < uf.length; i += 1){
+  estado.innerHTML += ` <option value=${uf[i]}>${uf[i]}</option>`
 }
 
 
-button1.addEventListener('click', ValidateEmail)
+const email = document.querySelector('#email')
+console.log(email)
+
+function validateEmail() {
+console.log(email.value)
+//   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//  if(inputText.value.match(mailformat)) {
+// // document.form1.text1.focus();
+// console.log(`ok`)
+// return true;
+
+// } else {
+// alert("You have entered an invalid email address!");
+// // document.form1.text1.focus();
+// console.log(`false`)
+// return false;
+// }
+}
+
+email.addEventListener('blur',validateEmail())
+console.log(email.value)
+// button1.addEventListener('click', ValidateEmail)
