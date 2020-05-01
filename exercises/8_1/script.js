4913eadd4778ed11e5fd331b78ff2ff0f56cf9da
Diego Matheus Campos <diegodiko05@gmail.com>
2020-05-01 12:57:29 -0300

    Exercise 3

--- exercises/8_1/script.js
+++ exercises/8_1/script.js
@@ -1,18 +1,8 @@
-// Crie uma função que receba um número e retorne seu fatorial.
-// Minha resoluçao
-// const fatorial = (num) => {
-//   if (num < 0){
-//     return `O valor deve ser maior ou igual a zero`
-//   } else if ( (num === 0) || (num === 1) ){
-//     return 1
-//   } else {
-//     let acumula = 1
-//     for (let i = num; i > 1; i -= 1) {
-//       acumula = acumula * i
-//       }
-//       return acumula
-//   }
-// }
-// codigo da internet 
-const fatorial = (n) => n === 0 || n === 1 ? 1 : n * fatorial(n-1);
-console.log(fatorial(4))
+const button = document.querySelector(".btn");
+const p = document.querySelector(".p1")
+let clickCount = 0
+console.log(button, p)
+button.addEventListener("click", () => {
+  clickCount += 1
+  p.innerHTML = `Já clicou ${clickCount} vez(es) consagrado(a)`
+})
