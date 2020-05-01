e92efd3d5336a76c8fac7deedd29be127f147d15
Diego Matheus Campos <diegodiko05@gmail.com>
2020-05-01 13:03:56 -0300

    Atualizando o exercicio 3 com humor

--- exercises/8_1/script.js
+++ exercises/8_1/script.js
@@ -4,5 +4,11 @@ let clickCount = 0
 console.log(button, p)
 button.addEventListener("click", () => {
   clickCount += 1
-  p.innerHTML = `Já clicou ${clickCount} vez(es) consagrado(a)`
+  if (clickCount < 10) {
+    p.innerHTML = `Já clicou ${clickCount} vez(es) consagrado(a)`
+  } else if (clickCount < 21) {
+    p.innerHTML = `Já são ${clickCount} cliques, não tem mais o que fazer?!`
+  } else {
+    p.innerHTML = `Cansei!! Só vou continuar contando porque sou obrigado via JS. ${clickCount} cliques na caralha do butão`
+  }
 })
