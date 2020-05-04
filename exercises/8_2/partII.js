45df89cc928db60a483c6e3e0f83fe37b69ebf0a
Diego Matheus Campos <diegodiko05@gmail.com>
2020-05-04 20:44:24 -0300

    Atualizaçao do bonus 2

--- exercises/8_2/partII.js
+++ exercises/8_2/partII.js
@@ -19,7 +19,7 @@ const lesson3 = {
 };
 // 1 Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, 
 // sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
- function modify (objt, key, value) {
+function modify(objt, key, value) {
   objt[key] = value;
   return objt;
 }
@@ -39,18 +39,18 @@ const values = (obj) => Object.values(obj)
 console.log(values(lesson3));
 // 5 Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. 
 // Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3.
-let allLessons = Object.assign({}, {lesson1, lesson2, lesson3 })
+let allLessons = Object.assign({}, { lesson1, lesson2, lesson3 })
 // allLessons.lesson1 = Object.assign({},lesson1)
 // allLessons.lesson2 = Object.assign({},lesson2)
 // allLessons.lesson3 = Object.assign({},lesson3)
 console.log(allLessons)
 // 6 Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 const totalEstudantes = (obj) => {
-let total = 0;
-for (item of Object.keys(obj)) {
-total += obj[item].numeroEstudantes
-}
-return total;
+  let total = 0;
+  for (item of Object.keys(obj)) {
+    total += obj[item].numeroEstudantes
+  }
+  return total;
 }
 console.log(totalEstudantes(allLessons))
 
@@ -59,20 +59,29 @@ console.log(totalEstudantes(allLessons))
 // (allLessons.lesson3.numeroEstudantes);
 // console.log(sum);
 // 7 Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
-function valorChave (object, indice) {
+function valorChave(object, indice) {
   return Object.values(object)[indice]
 }
 console.log(valorChave(lesson3, 0))
 // 8 Crie uma função que verifique se o par (chave / valor) existem na função. 
 // Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 
-const verification = (obj, key, value) => obj[key] === value ? true : false; 
+const verification = (obj, key, value) => obj[key] === value ? true : false;
 
 console.log(verification(lesson3, 'materia', 'Maria Clara'))
 
 // Bonus 1 :
 // Crie uma função para contar quantos estudantes assistiram as aulas de matemática. Use o objeto criado no exercício 5. allLessons
-const sum2 = (obj) => obj.lesson1.numeroEstudantes + obj.lesson3.numeroEstudantes
+const sum2 = (obj) => {
+  let total = 0;
+  let array = Object.keys(obj);
+  for (lesson of array) {
+    if (obj[lesson].materia === 'Matemática') {
+      total += obj[lesson].numeroEstudantes;
+    }
+  }
+  return total
+}
 
 console.log(sum2(allLessons))
 // Bonus 2 :
@@ -80,27 +89,27 @@ console.log(sum2(allLessons))
 // as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
 
 
-function createReport (obj, prof) {
+function createReport(obj, prof) {
   let professor = ''
   let aulas = []
   let estudantes = 0
-  if (obj.lesson1.professor === prof) {  
+  if (obj.lesson1.professor === prof) {
     professor = prof;
     aulas.push(obj.lesson1.materia)
     estudantes += obj.lesson1.numeroEstudantes
   }
-  if (obj.lesson2.professor === prof) {  
+  if (obj.lesson2.professor === prof) {
     professor = prof;
     aulas.push(obj.lesson2.materia)
     estudantes += obj.lesson2.numeroEstudantes
   }
-  if (obj.lesson3.professor === prof) { 
+  if (obj.lesson3.professor === prof) {
     professor = prof;
     aulas.push(obj.lesson3.materia)
     estudantes += obj.lesson3.numeroEstudantes
   }
 
-  const newArray = Object.assign({}, {professor, aulas, estudantes})
+  const newArray = Object.assign({}, { professor, aulas, estudantes })
   return newArray
 }
 console.log(createReport(allLessons, 'Maria Clara'))
