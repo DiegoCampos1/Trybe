f83d3d9f94012513c6f35ce6cb1eab0433d62f9e
Diego Campos <diegodiko05@gmail.com>
2020-06-03 12:17:41 -0300

    Botoes para filtro do pokemons por type, botoes de all, disabled quando tiver apenas 1 pokemon

--- exercises/12/exercises/exercise12_2/my-pokedex/src/App.js
+++ exercises/12/exercises/exercise12_2/my-pokedex/src/App.js
@@ -5,39 +5,70 @@ import Pokedex from './Pokedex';
 // import Pokemon from './Pokemon.js'
 
 let indice = 0;
+let pokemons2 = pokemons;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filter: pokemons[indice],
-      type: "",
+      filter: pokemons2[indice],
+      type: pokemons2,
+      disabled: false,
     };
   }
 
   next = () => {
     indice++;
-    if (indice >= pokemons.length) indice = 0;
-    return this.setState({ filter: pokemons[indice] });
+    if (indice >= this.state.type.length) indice = 0;
+    // console.log(this.state.filter)
+    return this.setState({ filter: pokemons2[indice] });
   };
 
-  fire= () => {
-    return this.setState({ type: "fire"})
-  }
+  filterPokemons = (typePokemon) => {
+    indice = 0;
+    const filterFire = pokemons.filter(
+      (pokemons) => pokemons.type === typePokemon,
+    );
+    pokemons2 = filterFire;
 
-  psycho= () => {
-    return this.setState({ type: "fire"})
-  }
+    pokemons2.length <= 1
+      ? this.setState({
+          type: pokemons2,
+          filter: pokemons2[indice],
+          disabled: true,
+        })
+      : this.setState({
+          type: pokemons2,
+          filter: pokemons2[indice],
+          disabled: false,
+        });
+  };
+
+  all = () => {
+    indice = 0;
+    pokemons2 = pokemons;
+    return this.setState({ type: pokemons2, filter: pokemons2[indice] , disabled: false });
+  };
 
   render() {
     return (
       <div className="App">
-        {console.log(this.state.filter)}
-        <button onClick={this.next}>Proximo Pokemon</button>
+        {/* {console.log(this.state.filter)} */}
+        <Pokedex todosPokemons={this.state.type} filter={this.state.filter} />
+        <button onClick={this.next} disabled={this.state.disabled}>
+          Next Pokemon
+        </button>
         {/* <button>Normal</button> */}
-        <button onClick={this.fire}>Fire</button>
-        <button onClick={this.psycho}>Psychic</button>
-        <Pokedex todosPokemons={pokemons} filter={this.state.filter} />
+        <button onClick={this.all}>All</button>
+        <button onClick={() => this.filterPokemons('Fire')}>Fire</button>
+        <button onClick={() => this.filterPokemons('Psychic')}>Psychic</button>
+        <button onClick={() => this.filterPokemons('Electric')}>
+          Electric
+        </button>
+        <button onClick={() => this.filterPokemons('Bug')}>Bug</button>
+        <button onClick={() => this.filterPokemons('Poison')}>Poison</button>
+        <button onClick={() => this.filterPokemons('Normal')}>Normal</button>
+        <button onClick={() => this.filterPokemons('Dragon')}>Dragon</button>
       </div>
     );
   }
