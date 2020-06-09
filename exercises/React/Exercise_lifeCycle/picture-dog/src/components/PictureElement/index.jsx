3f1504d5efab412d72a86e3f64198ebf24e7b8d8
Diego Campos <diegodiko05@gmail.com>
2020-06-09 20:02:03 -0300

    Fazendo a requisiçao de API de mandei random e implementadno um Loading enquanto existe o carregamento

--- exercises/React/Exercise_lifeCycle/picture-dog/src/components/PictureElement/index.jsx
+++ exercises/React/Exercise_lifeCycle/picture-dog/src/components/PictureElement/index.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 
 class PictureElement extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: '',
+      isLoaded: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            item: result.message,
+          });
+        },
+        // Nota: É importante lidar com os erros aqui
+        // em vez de um bloco catch() para não recebermos
+        // exceções de erros dos componentes.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
   render() {
-    return (
-      <div>
-        <img src="https://images.dog.ceo/breeds/bulldog-french/n02108915_5306.jpg" />
-      </div>
-    );
+    const { error, isLoaded, item } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return <img src={item} />;
+    }
   }
 }
 
