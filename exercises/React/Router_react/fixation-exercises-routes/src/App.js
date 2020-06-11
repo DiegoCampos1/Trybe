6399582886bb723d8df9da83b0ae572d0035e05c
Diego Campos <diegodiko05@gmail.com>
2020-06-11 11:25:21 -0300

    Fim do exercicio parte I do dia 13.3

--- exercises/React/Router_react/fixation-exercises-routes/src/App.js
+++ exercises/React/Router_react/fixation-exercises-routes/src/App.js
@@ -1,7 +1,9 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
 import Home from './Home';
-import { BrowserRouter, Link } from 'react-router-dom';
+import Users from './Users';
+import About from './About';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -15,9 +17,19 @@ class App extends Component {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/users"> Users</Link>
+            <Link to="/users">Users</Link>
           </li>
         </ol>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route  path='/about' component={About} />
+        <Route path='/Users/:ID' component={Users} />
+        <Route  
+        path='/users' 
+        render={props => <Users {...props} greetingMessage={"Bom dia Consagrado"} />} 
+        />
+      </Switch>
       </BrowserRouter>
     );
   }
--- exercises/React/Router_react/fixation-exercises-routes/src/Users.jsx
+++ exercises/React/Router_react/fixation-exercises-routes/src/Users.jsx
@@ -1,11 +1,21 @@
 // arquivo Users.js
 import React from 'react';
 
-const Users = () => (
-  <div>
-    <h2> Users </h2>
-    <p> My awesome Users component </p>
-  </div>
-);
+class Users extends React.Component {
+  render() {
+    const { greetingMessage = 'Hi There' } = this.props
+    const ID = this.props.match.params.ID;
+
+
+
+    return (
+      <div>
+        {console.log(ID)}
+        <h2> Users </h2>
+        <p> {greetingMessage}, this is my awesome Users component </p>
+      </div>
+    );
+  }
+}
 
 export default Users;
