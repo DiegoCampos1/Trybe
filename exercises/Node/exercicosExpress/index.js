8bb4a5cfc5f5d918b65dfd3d0efa5c8c6cfbd73a
Diego Campos <diegodiko05@gmail.com>
2020-09-25 18:56:33 -0300

    Exercicio de Node

--- exercises/Node/exercicosExpress/index.js
+++ exercises/Node/exercicosExpress/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+
 const cryptoHandle = () => {
   return crypto.randomBytes(12).toString('hex');
 };
+
 const validateEmail = (emailAdress) => {
   const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
   if (emailAdress.match(regexEmail)) {
@@ -11,10 +13,7 @@ const validateEmail = (emailAdress) => {
   }
   return false;
 };
-app.get('/', (req, res, next) => {
-  console.log('Entrei');
-  res.json({ message: 'Hello world' });
-});
+
 const validatePassword = (password) => {
   const validation = password.split('').every((char) => {
     typeof char === Number;
@@ -22,14 +21,14 @@ const validatePassword = (password) => {
   if (password.length >= 4 && password.length <= 8 && validation) return true;
   return false;
 };
+
 app.post('/login', (req, res, next) => {
-  console.log(req.query);
   const { password, email } = req.query;
   const hash = cryptoHandle();
-  if (validateEmail(email) && validatePassword(password))
-    return res.status(200).send(hash);
+  if (validateEmail(email) && validatePassword(password)) return res.status(200).send(hash);
   res.status(403).send('E-mail or password is not correct');
 });
+
 app.listen(3000, () => {
   console.log('O pai tá ON!');
 });
